a2f7014dff8a873f400a46be7d3b07f5064dc7d8
Remi van der Laan <rrm-remi@hotmail.com>
2021-05-19 19:46:59 +0200

    Macroquad progress (better cam movement)

--- b2d-core/src/main.rs
+++ b2d-core/src/main.rs
@@ -1,7 +1,7 @@
 use macroquad::prelude::*;
 
 mod scene;
-use scene::{scene_model::*};
+use scene::scene_model::*;
 
 fn short_angle_dist(a0: f32, a1: f32) -> f32 {
   let max = 360.0;
@@ -20,53 +20,64 @@ fn draw_cross(x: f32, y: f32, color: Color) {
   draw_line(x, y - size, x, y + size, thickness, color);
 }
 
-fn update() {
-
-}
+fn update() {}
 
 #[macroquad::main("New scene* • B2D")]
 async fn main() {
-    let mut model = SceneModel::create();
-    let scene = &mut model.scene;
+  let mut model = SceneModel::create();
+  let scene = &mut model.scene;
 
-    let mut screen_rect = Rect{x: 0., y: 0., w: screen_width(), h: screen_height()};
-    let mut cam2d = Camera2D::from_display_rect(screen_rect);
-    set_camera(&cam2d);
+  let mut screen_rect = Rect {
+    x: 0.,
+    y: 0.,
+    w: screen_width(),
+    h: screen_height(),
+  };
+  set_camera(scene.main_camera.set_resolution(screen_rect));
 
-    // JS bindings are available: https://github.com/not-fl3/quad-net/tree/master/examples
-    // could do web-rtc after all. Or do it through PeerJS
+  // JS bindings are available: https://github.com/not-fl3/quad-net/tree/master/examples
+  // could do web-rtc after all. Or do it through PeerJS
 
-    // TODO: next-up: create UI.
-    // - Menu for adding shape
-    // - List/tree of shapes
-    // - UI focus context: clicking in world vs. clicking in UI
-    // - Move shape points
-    // https://github.com/not-fl3/macroquad/blob/master/examples/events.rs
-    // https://github.com/not-fl3/macroquad/blob/master/examples/ui.rs
+  // TODO: next-up: create UI.
+  // - Menu for adding shape
+  // - List/tree of shapes
+  // - UI focus context: clicking in world vs. clicking in UI
+  // - Move shape points
+  // https://github.com/not-fl3/macroquad/blob/master/examples/events.rs
+  // https://github.com/not-fl3/macroquad/blob/master/examples/ui.rs
 
-    loop {
-        scene.update();
+  loop {
+    scene.update();
 
-        clear_background(RED);
+    clear_background(RED);
 
-        screen_rect.w = screen_width();
-        screen_rect.h = screen_height();
+    screen_rect.w = screen_width();
+    screen_rect.h = screen_height();
 
-        cam2d.offset = -scene.main_camera.transform.location;
-        cam2d.rotation = scene.main_camera.transform.rotation;
-        cam2d.zoom = vec2(1. / screen_rect.w * 2., -1. / screen_rect.h * 2.) * scene.main_camera.zoom;
-        set_camera(&cam2d);
-        
-        draw_line(40.0, 40.0, 100.0, 200.0, 15.0, BLUE);
-        draw_rectangle(screen_width() / 2.0 - 60.0, 100.0, 120.0, 60.0, GREEN);
-        draw_circle(screen_width() - 30.0, screen_height() - 30.0, 15.0, YELLOW);
+    // cam2d.offset = -scene.main_camera.transform.location;
+    // cam2d.rotation = scene.main_camera.transform.rotation;
+    // cam2d.zoom = vec2(1. / screen_rect.w * 2., -1. / screen_rect.h * 2.) * scene.main_camera.zoom;
+    // set_camera(scene.main_camera.set_resolution(screen_rect));
 
-        draw_text("IT WORKS!", 20.0, 20.0, 30.0, DARKGRAY);
-        
-        set_default_camera();
-        draw_text(&format!("x {:.2}, y{:.2}", scene.main_camera.transform.location.x, scene.main_camera.transform.location.y), 20.0, 20.0, 30.0, DARKGRAY);
+    draw_line(40.0, 40.0, 100.0, 200.0, 15.0, BLUE);
+    draw_rectangle(screen_width() / 2.0 - 60.0, 100.0, 120.0, 60.0, GREEN);
+    draw_circle(screen_width() - 30.0, screen_height() - 30.0, 15.0, YELLOW);
 
+    draw_text("IT WORKS!", 20.0, 20.0, 30.0, DARKGRAY);
 
-        next_frame().await
-    }
+    set_default_camera();
+    draw_text(
+      &format!(
+        "x {:.2}, y{:.2}",
+        scene.main_camera.cam2d().target.x,
+        scene.main_camera.cam2d().target.y
+      ),
+      20.0,
+      20.0,
+      30.0,
+      DARKGRAY,
+    );
+
+    next_frame().await
+  }
 }
--- b2d-core/src/scene/scene_model.rs
+++ b2d-core/src/scene/scene_model.rs
@@ -13,86 +13,100 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Clone, Debug)]
 pub struct Transform {
-    pub location: Vec2,
-    pub rotation: f32,
-    pub scale: Vec2,
+  pub location: Vec2,
+  pub rotation: f32,
+  pub scale: Vec2,
 }
 
 impl Default for Transform {
-    fn default() -> Transform {
-        Transform {
-            location: Vec2::default(),
-            rotation: 0.,
-            scale: vec2(1., 1.),
-        }
-   }
+  fn default() -> Transform {
+    Transform {
+      location: Vec2::default(),
+      rotation: 0.,
+      scale: vec2(1., 1.),
+    }
+  }
 }
 
 #[derive(Clone, Debug)]
 pub struct Shape {
   id: i32,
   transform: Transform,
-//   name: str,
+  //   name: str,
   points: Vec<Vec2>,
-//   edges/faces?
+  //   edges/faces?
   // layer ?
 }
 
-#[derive(Clone, Debug)]
+#[derive(Clone)]
 pub struct Camera {
+  _camera: Camera2D,
   pub transform: Transform,
   pub zoom: f32,
   cursor_grab: bool,
+  prev_mouse_pos: Vec2,
 }
 
 impl Default for Camera {
   fn default() -> Camera {
-      Camera {
-          transform: Transform::default(),
-          zoom: 1.,
-          cursor_grab: true,
-      }
- }
+    Camera {
+      _camera: Camera2D::default(),
+      transform: Transform::default(),
+      zoom: 1.,
+      cursor_grab: true,
+      prev_mouse_pos: Vec2::default(),
+    }
+  }
 }
 
 impl Camera {
+  pub fn set_resolution(&mut self, rect: Rect) -> &Camera2D {
+    // self._camera.
+    self._camera = Camera2D::from_display_rect(rect);
+    self._camera.target = Vec2::default();
+    return &self._camera;
+  }
 
+  pub fn cam2d(&self) -> &Camera2D {
+    return &self._camera;
+  }
+}
+
+trait Updatable {
+  fn update(&mut self);
+}
+
+impl Updatable for Camera {
   // would be nice to have initialize() for cursor grab
 
   fn update(&mut self) {
-
     // Based on
     // https://github.com/not-fl3/macroquad/blob/master/examples/camera_transformations.rs
-    if is_key_down(KeyCode::W) {
-      self.transform.location.y += 0.03;
+    let mut local_translation = Vec2::default();
+    if is_key_down(KeyCode::Up) {
+      local_translation.y -= 10.;
     }
-    if is_key_down(KeyCode::S) {
-      self.transform.location.y -= 0.03;
+    if is_key_down(KeyCode::Down) {
+      local_translation.y += 10.;
     }
-    if is_key_down(KeyCode::A) {
-      self.transform.location.x -= 0.03;
+    if is_key_down(KeyCode::Left) {
+      local_translation.x -= 10.;
     }
-    if is_key_down(KeyCode::D) {
-      self.transform.location.x += 0.03;
+    if is_key_down(KeyCode::Right) {
+      local_translation.x += 10.;
     }
+    // TODO: Rotation
+    self.transform.location += local_translation / self.zoom;
 
-    // pan when cursor is at screen edge?
-    let pan_border_width = 8.; 
     let mouse_pos: Vec2 = mouse_position().into();
-    let pan_speed = 0.01;
-    
 
-    if mouse_pos.x < pan_border_width {
-      self.transform.location.x -= pan_speed;
-    }
-    if mouse_pos.y < pan_border_width {
-      self.transform.location.y += pan_speed;
-    }
-    if mouse_pos.x > screen_width() - pan_border_width && mouse_pos.x <= screen_width() {
-      self.transform.location.x += pan_speed;
-    }
-    if mouse_pos.y > screen_height() - pan_border_width && mouse_pos.y <= screen_height() {
-      self.transform.location.y -= pan_speed;
+    // Move with Shift + middle mouse button
+    if is_key_down(KeyCode::LeftShift) && is_mouse_button_down(MouseButton::Middle) {
+      let p1 = self._camera.screen_to_world(self.prev_mouse_pos);
+      let p2 = self._camera.screen_to_world(mouse_pos);
+      let delta = p2 - p1;
+
+      self.transform.location -= delta;
     }
 
     if is_key_pressed(KeyCode::Escape) {
@@ -102,32 +116,49 @@ impl Camera {
 
     match mouse_wheel() {
       (_x, y) if y != 0.0 => {
-          // Normalize mouse wheel values is browser (chromium: 53, firefox: 3)
-          #[cfg(target_arch = "wasm32")]
-          let y = if y < 0.0 {
-              -1.0
-          } else if y > 0.0 {
-              1.0
-          } else {
-              0.0
-          };
-          if is_key_down(KeyCode::LeftControl) {
-              self.zoom *= 1.1f32.powf(y);
-          } else {
-              self.transform.rotation += 10.0 * y;
-              self.transform.rotation = match self.transform.rotation {
-                  angle if angle >= 360.0 => angle - 360.0,
-                  angle if angle < 0.0 => angle + 360.0,
-                  angle => angle,
-              }
-          }
+        // Normalize mouse wheel values is browser (chromium: 53, firefox: 3)
+        #[cfg(target_arch = "wasm32")]
+        let y = if y < 0.0 {
+          -1.0
+        } else if y > 0.0 {
+          1.0
+        } else {
+          0.0
+        };
+        // if is_key_down(KeyCode::LeftControl) {
+        // TODO: Zoom to cursor
+        self.zoom *= 1.1f32.powf(y);
+        // } else {
+        // self.transform.rotation += 10.0 * y;
+        // self.transform.rotation = match self.transform.rotation {
+        //     angle if angle >= 360.0 => angle - 360.0,
+        //     angle if angle < 0.0 => angle + 360.0,
+        //     angle => angle,
+        // }
+        // }
       }
       _ => (),
-  }
+    }
+
+    // Deal with window resizing
+    let rect = Rect {
+      x: 0.,
+      y: 0.,
+      w: screen_width(),
+      h: screen_height(),
+    };
+    self._camera = Camera2D::from_display_rect(rect);
+    self._camera.target = self.transform.location;
+    self._camera.rotation = self.transform.rotation;
+    self._camera.zoom = vec2(1. / rect.w * 2., -1. / rect.h * 2.) * self.zoom;
+
+    set_camera(&self._camera);
+
+    self.prev_mouse_pos = mouse_pos
   }
 }
 
-#[derive(Clone, Debug, Default)]
+#[derive(Clone, Default)]
 pub struct Scene {
   pub shapes: Vec<Shape>,
   pub main_camera: Camera,
@@ -141,261 +172,248 @@ impl Scene {
 
 pub struct ActionHandler {
   // undoStack Vec<Action>
-  // TODO: take in actions, store undo/redo
-  // Also accept middleware: push actions over webRTC (or just send the whole scene for now)
-  // maybe call it Operations instead of actions?
+// TODO: take in actions, store undo/redo
+// Also accept middleware: push actions over webRTC (or just send the whole scene for now)
+// maybe call it Operations instead of actions?
 }
 
 impl ActionHandler {
-  fn runAction() {
-
-  }
-  fn undo() {
-
-  }
-  fn redo() {
-
-  }
+  fn runAction() {}
+  fn undo() {}
+  fn redo() {}
 }
- 
+
 // the "main" manager
 pub struct SceneModel {
-//   web_rtc_manager: Rc<RefCell<WebRTCManager>>,
+  //   web_rtc_manager: Rc<RefCell<WebRTCManager>>,
   pub scene: Scene, // shouldn't be public, but, yeah.
 
-//   value: String, // TODO: what is "value"?
+                    //   value: String, // TODO: what is "value"?
 }
 
 //
 #[derive(Clone, Debug, Eq, PartialEq)]
 pub enum MessageSender {
-    Me,
-    Other,
+  Me,
+  Other,
 }
 
 #[derive(Clone, Debug)]
 pub struct Message {
-    sender: MessageSender,
-    content: String,
+  sender: MessageSender,
+  content: String,
 }
 
 impl Message {
-    pub fn new(content: String, sender: MessageSender) -> Message {
-        Message {
-            content: content,
-            sender: sender,
-        }
+  pub fn new(content: String, sender: MessageSender) -> Message {
+    Message {
+      content: content,
+      sender: sender,
     }
+  }
 }
 
 // All action types (at least, those sent over WebRTC)
-#[derive(Clone, Debug)]
+#[derive(Clone)]
 pub enum Msg {
-    StartAsServer,
-    ConnectToServer,
-    // UpdateWebRTCState(State),
-    Disconnect,
-    Send,
-    UpdateScene(Scene), // TODO: temporary msg
-    // UpdateInputValue(String),
-    // UpdateInputChatValue(String),
-    // OnKeyUp(KeyboardEvent),
-    // CopyToClipboard,
-    ValidateOffer,
-    ResetWebRTC,
+  StartAsServer,
+  ConnectToServer,
+  // UpdateWebRTCState(State),
+  Disconnect,
+  Send,
+  UpdateScene(Scene), // TODO: temporary msg
+  // UpdateInputValue(String),
+  // UpdateInputChatValue(String),
+  // OnKeyUp(KeyboardEvent),
+  // CopyToClipboard,
+  ValidateOffer,
+  ResetWebRTC,
 }
 
 impl SceneModel {
+  pub fn create() -> Self {
+    // let mut web_rtc_manager = WebRTCManager::create_default();
 
-    pub fn create() -> Self {
-        // let mut web_rtc_manager = WebRTCManager::create_default();
+    // let rc = Rc::new(RefCell::new(web_rtc_manager));
 
-        // let rc = Rc::new(RefCell::new(web_rtc_manager));
+    let model = SceneModel {
+      // web_rtc_manager: rc.clone(),
+      scene: Scene {
+        main_camera: Camera::default(),
+        shapes: vec![],
+      },
+      // value: "".into(),
+      // chat_value: "".into(),
+      // node_ref: NodeRef::default(),
+    };
 
-        let model = SceneModel {
-            // web_rtc_manager: rc.clone(),
-            scene: Scene{
-                main_camera: Camera::default(),
-                shapes: vec!(),
-            },
-            // value: "".into(),
-            // chat_value: "".into(),
-            // node_ref: NodeRef::default(),
-        };
+    // let mut model_callback = |msg: Msg| {
+    //     model.update(msg);
+    // };
 
-        // let mut model_callback = |msg: Msg| {
-        //     model.update(msg);
-        // };
+    // web_rtc_manager.set_model_callback(model_callback);
 
-        // web_rtc_manager.set_model_callback(model_callback);
-
-        model
+    model
   }
 
   pub fn get_scene(self) -> Rc<Scene> {
-      Rc::new(self.scene)
+    Rc::new(self.scene)
   }
 
   pub fn update(&mut self) {
     self.scene.update();
   }
 
-//   pub fn update(&mut self, msg: Msg) -> bool {
-//     match msg {
-//       Msg::StartAsServer => {
-//         self.web_rtc_manager
-//             .borrow_mut()
-//             .set_state(State::Server(ConnectionState::new()));
-//         WebRTCManager::start_web_rtc(self.web_rtc_manager.clone());
-//         let re_render = true;
-//         return re_render;
-//     }
+  //   pub fn update(&mut self, msg: Msg) -> bool {
+  //     match msg {
+  //       Msg::StartAsServer => {
+  //         self.web_rtc_manager
+  //             .borrow_mut()
+  //             .set_state(State::Server(ConnectionState::new()));
+  //         WebRTCManager::start_web_rtc(self.web_rtc_manager.clone());
+  //         let re_render = true;
+  //         return re_render;
+  //     }
 
-//     Msg::ConnectToServer => {
-//         self.web_rtc_manager
-//             .borrow_mut()
-//             .set_state(State::Client(ConnectionState::new()));
-//         WebRTCManager::start_web_rtc(self.web_rtc_manager.clone());
-//         let re_render = true;
-//         return re_render;
-//     }
+  //     Msg::ConnectToServer => {
+  //         self.web_rtc_manager
+  //             .borrow_mut()
+  //             .set_state(State::Client(ConnectionState::new()));
+  //         WebRTCManager::start_web_rtc(self.web_rtc_manager.clone());
+  //         let re_render = true;
+  //         return re_render;
+  //     }
 
-//     Msg::UpdateWebRTCState(web_rtc_state) => {
-//       self.value = "".into();
-//       let debug = SceneModel::get_debug_state_string(&web_rtc_state);
-//       console::log_1(&debug.into());
+  //     Msg::UpdateWebRTCState(web_rtc_state) => {
+  //       self.value = "".into();
+  //       let debug = SceneModel::get_debug_state_string(&web_rtc_state);
+  //       console::log_1(&debug.into());
 
-//       // let debug = self.get_serialized_offer_and_candidates();
-//       // let hash = hmac_sha256::Hash::hash(debug.as_bytes());
-//       // let hash_as_string = hex::encode(hash);
-//       // console::log_1(&hash_as_string.into());
+  //       // let debug = self.get_serialized_offer_and_candidates();
+  //       // let hash = hmac_sha256::Hash::hash(debug.as_bytes());
+  //       // let hash_as_string = hex::encode(hash);
+  //       // console::log_1(&hash_as_string.into());
 
-//       let re_render = true;
-//       return re_render;
-//   }
+  //       let re_render = true;
+  //       return re_render;
+  //   }
 
-//   Msg::ResetWebRTC => {
-//       let web_rtc_manager = WebRTCManager::create_default();
-//       let rc = Rc::new(RefCell::new(web_rtc_manager));
-//       self.web_rtc_manager = rc;
-//       self.scene = Scene::default();
-//       self.value = "".into();
+  //   Msg::ResetWebRTC => {
+  //       let web_rtc_manager = WebRTCManager::create_default();
+  //       let rc = Rc::new(RefCell::new(web_rtc_manager));
+  //       self.web_rtc_manager = rc;
+  //       self.scene = Scene::default();
+  //       self.value = "".into();
 
-//       let re_render = true;
-//       return re_render;
-//   }
+  //       let re_render = true;
+  //       return re_render;
+  //   }
 
-// //   Msg::UpdateInputValue(val) => {
-// //       self.value = val;
-// //       let re_render = true;
-// //       return re_render;
-// //   }
+  // //   Msg::UpdateInputValue(val) => {
+  // //       self.value = val;
+  // //       let re_render = true;
+  // //       return re_render;
+  // //   }
 
-//   Msg::ValidateOffer => {
-//     let state = self.web_rtc_manager.borrow().get_state();
+  //   Msg::ValidateOffer => {
+  //     let state = self.web_rtc_manager.borrow().get_state();
 
-//     match state {
-//         State::Server(_connection_state) => {
-//             let result = WebRTCManager::validate_answer(
-//                 self.web_rtc_manager.clone(),
-//                 &self.value,
-//             );
+  //     match state {
+  //         State::Server(_connection_state) => {
+  //             let result = WebRTCManager::validate_answer(
+  //                 self.web_rtc_manager.clone(),
+  //                 &self.value,
+  //             );
 
-//             if result.is_err() {
-//                 web_sys::Window::alert_with_message(
-//                     &web_sys::window().unwrap(),
-//                     &format!(
-//                         "Cannot use answer. Failure reason: {:?}",
-//                         result.err().unwrap()
-//                     ),
-//                 )
-//                 .expect("alert should work");
-//             }
-//         }
-//         _ => {
-//             let result = WebRTCManager::validate_offer(
-//                 self.web_rtc_manager.clone(),
-//                 &self.value,
-//             );
+  //             if result.is_err() {
+  //                 web_sys::Window::alert_with_message(
+  //                     &web_sys::window().unwrap(),
+  //                     &format!(
+  //                         "Cannot use answer. Failure reason: {:?}",
+  //                         result.err().unwrap()
+  //                     ),
+  //                 )
+  //                 .expect("alert should work");
+  //             }
+  //         }
+  //         _ => {
+  //             let result = WebRTCManager::validate_offer(
+  //                 self.web_rtc_manager.clone(),
+  //                 &self.value,
+  //             );
 
-//             if result.is_err() {
-//                 web_sys::Window::alert_with_message(
-//                     &web_sys::window().unwrap(),
-//                     &format!(
-//                         "Cannot use offer. Failure reason: {:?}",
-//                         result.err().unwrap()
-//                     ),
-//                 )
-//                 .expect("alert should work");
-//             }
-//         }
-//     };
+  //             if result.is_err() {
+  //                 web_sys::Window::alert_with_message(
+  //                     &web_sys::window().unwrap(),
+  //                     &format!(
+  //                         "Cannot use offer. Failure reason: {:?}",
+  //                         result.err().unwrap()
+  //                     ),
+  //                 )
+  //                 .expect("alert should work");
+  //             }
+  //         }
+  //     };
 
-//     let re_render = true;
-//     return re_render;
-// }
+  //     let re_render = true;
+  //     return re_render;
+  // }
 
-// // Msg::Send => {
-// //     let content = self.chat_value.clone();
-// //     let my_message = Message::new(content.clone(), MessageSender::Me);
-// //     self.messages.push(my_message);
-// //     self.web_rtc_manager.borrow().send_message(content);
-// //     self.chat_value = "".into();
-// //     self.scroll_top();
-// //     let re_render = true;
-// //     return re_render;
-// // }
+  // // Msg::Send => {
+  // //     let content = self.chat_value.clone();
+  // //     let my_message = Message::new(content.clone(), MessageSender::Me);
+  // //     self.messages.push(my_message);
+  // //     self.web_rtc_manager.borrow().send_message(content);
+  // //     self.chat_value = "".into();
+  // //     self.scroll_top();
+  // //     let re_render = true;
+  // //     return re_render;
+  // // }
 
-// Msg::Disconnect => {
-//     // TODO: Re-construct the scene?
-//     let web_rtc_manager = WebRTCManager::create_default();
-//     let rc = Rc::new(RefCell::new(web_rtc_manager));
-//     self.web_rtc_manager = rc;
-//     self.value = "".into();
-//     let re_render = true;
-//     return re_render;
-// }
-//         Msg::UpdateScene(_) => {
-//             return false
-//         }
-//         Msg::Send => {
-//             return false
-//         }
-//     }
-//   }
+  // Msg::Disconnect => {
+  //     // TODO: Re-construct the scene?
+  //     let web_rtc_manager = WebRTCManager::create_default();
+  //     let rc = Rc::new(RefCell::new(web_rtc_manager));
+  //     self.web_rtc_manager = rc;
+  //     self.value = "".into();
+  //     let re_render = true;
+  //     return re_render;
+  // }
+  //         Msg::UpdateScene(_) => {
+  //             return false
+  //         }
+  //         Msg::Send => {
+  //             return false
+  //         }
+  //     }
+  //   }
 
+  //   fn get_debug_state_string(state: &State) -> String {
+  //     match state {
+  //         State::DefaultState => "Default State".into(),
+  //         State::Server(connection_state) => format!(
+  //             "{}\nice gathering: {:?}\nice connection: {:?}\ndata channel: {:?}\n",
+  //             "Server",
+  //             connection_state.ice_gathering_state,
+  //             connection_state.ice_connection_state,
+  //             connection_state.data_channel_state,
+  //         ),
 
-//   fn get_debug_state_string(state: &State) -> String {
-//     match state {
-//         State::DefaultState => "Default State".into(),
-//         State::Server(connection_state) => format!(
-//             "{}\nice gathering: {:?}\nice connection: {:?}\ndata channel: {:?}\n",
-//             "Server",
-//             connection_state.ice_gathering_state,
-//             connection_state.ice_connection_state,
-//             connection_state.data_channel_state,
-//         ),
-
-//         State::Client(connection_state) => format!(
-//             "{}\nice gathering: {:?}\nice connection: {:?}\ndata channel: {:?}\n",
-//             "Client",
-//             connection_state.ice_gathering_state,
-//             connection_state.ice_connection_state,
-//             connection_state.data_channel_state,
-//         ),
-//     }
-// }
-
-
-
+  //         State::Client(connection_state) => format!(
+  //             "{}\nice gathering: {:?}\nice connection: {:?}\ndata channel: {:?}\n",
+  //             "Client",
+  //             connection_state.ice_gathering_state,
+  //             connection_state.ice_connection_state,
+  //             connection_state.data_channel_state,
+  //         ),
+  //     }
+  // }
 }
-
 
 // TODO:
 // - Step 0: Set up connection. Looks fairly simple in YewWebRTC chat repo
 // - Step 1: Send entire serialized scene to peers on any change
 // - Step 2: Only send actions, modify own state (look into CFRDT)
-
 
 // Copied here onwards
 // #[derive(Serialize, Deserialize, Debug, Clone)]
